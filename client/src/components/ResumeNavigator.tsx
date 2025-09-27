7789953ef479019efd9e5b6509cdacd9747542c4
elo369 <rohitrikam88@gmail.com>
2025-09-27 23:59:21 +0530

    responsive

--- client/src/components/ResumeNavigator.tsx
+++ client/src/components/ResumeNavigator.tsx
@@ -1,5 +1,7 @@
 // import React from "react";
 
+import { useState } from "react";
+import { motion } from "motion/react";
 const sections = [
   "PersonalInfo",
   "Education",
@@ -10,25 +12,44 @@ const sections = [
 ];
 
 export default function ResumeSidebar() {
+  const [view, setView] = useState(false);
+
   return (
-    <div className="w-40 bg-orange-200 min-h-20 p-3  shadow-md rounded-lg">
-      <h2 className="text-sm font-bold mb-2 ">Resume Sections Navigator</h2>
-      <ul className="space-y-3">
-        {sections.map((section, index) => (
-          <li
-            key={index}
-            // onClick={() => onSelect(section)}
-            className={`flex items-center gap-1 p-2 rounded-lg cursor-pointer transition h-3 w-3  bg-amber-400 translate-2`
-        }
-          >
-            <a href={`#${section}`} className="font-medium font-poppins">
-            {/* Circle before section name */}
-           
-            {section}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div
+        className="  h-7 w-7 rounded-full bg-black flex items-center justify-center absolute right-2 top-2"
+        onClick={() => setView(!view)}
+      >
+        👇🏻
+      </div>
+      {view && (
+        <motion.div
+        
+          transition={{
+            duration: 0.9,
+            delay: 2,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="w-40 bg-orange-200 min-h-20 p-3  shadow-md rounded-lg transition-all ease-in-out "
+        >
+          <h2 className="text-sm font-bold mb-2 ">Resume Sections Navigator</h2>
+          <ul className="space-y-3">
+            {sections.map((section, index) => (
+              <li
+                key={index}
+                // onClick={() => onSelect(section)}
+                className={`flex items-center gap-1 p-2 rounded-lg cursor-pointer transition h-3 w-3  bg-amber-400 translate-2`}
+              >
+                <a href={`#${section}`} className="font-medium font-poppins">
+                  {/* Circle before section name */}
+
+                  {section}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      )}
     </div>
   );
 }
--- client/src/pages/inputResume.tsx
+++ client/src/pages/inputResume.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import RealInput from "../components/RealInput.tsx";
 import WrapDiv from "../components/WrapDiv.tsx";
 import { useDispatch } from "react-redux";
@@ -18,9 +18,8 @@ import { updateDataResume } from "../store/slice/resume/resume.slice.ts";
 import ResumeSidebar from "../components/ResumeNavigator.tsx";
 
 const InputResume = () => {
-  
   const [inputValue, setInputValue] = useState<{ [key: string]: string }>({});
-      let dispatch = useDispatch<AppDispatch>()
+  let dispatch = useDispatch<AppDispatch>();
 
   let collectDatas = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e);
@@ -30,77 +29,74 @@ const InputResume = () => {
     }));
   };
 
-  useEffect(()=>{
-    dispatch(updateDataResume({technicalSkills:inputValue}))
-  },[inputValue])
+  useEffect(() => {
+    dispatch(updateDataResume({ technicalSkills: inputValue }));
+  }, [inputValue]);
 
   return (
     <>
-    <button className="w-20 h-7 bg-blue-500 text-white font-bold font-poppins absolute left-5 top-7  rounded-2xl">
-      <a href="/">
-      Back
-      </a>
-    </button>
-    <div className="sticky top-5 right-5 ">
-    <div className="absolute  top-5 right-5 ">
-      <ResumeSidebar/>
-    </div>
-    </div>
-    <div className="w-full px-20 z-1">
-      <div id="PersonalInfo">
-        <NameInputs/>
-       
+      <button className="w-20 h-7 bg-blue-500 text-white font-bold font-poppins absolute left-5 top-7  rounded-2xl">
+        <a href="/">Back</a>
+      </button>
+      <div className="sticky top-5 right-5 ">
+        <div className="absolute  top-5 right-5 ">
+          <ResumeSidebar />
+        </div>
       </div>
+      <div className="w-full px-20 z-1">
+        <div id="PersonalInfo">
+          <NameInputs />
+        </div>
 
-      
+        <div id="Education">
+          <EducationInputs />
+        </div>
 
-      <div id="Education">
-        <EducationInputs/>
-      </div>
+        <div id="Achievements">
+          <AchievementsInputs />
+        </div>
 
-      <div id="Achievements">
-        <AchievementsInputs/>
-      </div>
+        <div id="Experience">
+          <Experience />
+        </div>
 
-      <div id="Experience">
-        <Experience/>
-      </div>
-
-      <div id="Projects">
-        <Project/>
-      </div>
+        <div id="Projects">
+          <Project />
+        </div>
 
         <WrapDiv id="TechnicalSkills">
-      <div id="mainTechicalSkills " className="w-full justify-start md:flex md:grid-cols-none grid grid-cols-1">
-          <RealInput
-            type="text"
-            name="language"
-            label="language"
-            placeholder="Programming Languages (e.g., JavaScript, Python)"
-            value={inputValue.language || ""}
-            onChange={collectDatas}
-          />
-          <RealInput
-            type="text"
-            name="devTools"
-            label="devTools"
-            placeholder="Development Tools (e.g., VS Code, Git)"
-            value={inputValue.devTools || ""}
-            onChange={collectDatas}
-          />
-          <RealInput
-            type="url"
-            name="techFameworks"
-            label="tech Fameworks"
-            placeholder="Frameworks/Libraries (e.g., React, Express.js)"
-            value={inputValue.techFameworks || ""}
-            onChange={collectDatas}
-          />
-      </div>
+          <div
+            id="mainTechicalSkills "
+            className="w-full justify-start md:flex md:grid-cols-none grid grid-cols-1"
+          >
+            <RealInput
+              type="text"
+              name="language"
+              label="language"
+              placeholder="Programming Languages (e.g., JavaScript, Python)"
+              value={inputValue.language || ""}
+              onChange={collectDatas}
+            />
+            <RealInput
+              type="text"
+              name="devTools"
+              label="devTools"
+              placeholder="Development Tools (e.g., VS Code, Git)"
+              value={inputValue.devTools || ""}
+              onChange={collectDatas}
+            />
+            <RealInput
+              type="url"
+              name="techFameworks"
+              label="tech Fameworks"
+              placeholder="Frameworks/Libraries (e.g., React, Express.js)"
+              value={inputValue.techFameworks || ""}
+              onChange={collectDatas}
+            />
+          </div>
         </WrapDiv>
-        
-    </div>
-  </>
+      </div>
+    </>
   );
 };
 
