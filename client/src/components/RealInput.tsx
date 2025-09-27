e40b21b25377157550d972f624a1c6199b33e005
elo369 <rohitrikam88@gmail.com>
2025-09-28 01:01:02 +0530

    djsjd

--- client/src/components/RealInput.tsx
+++ client/src/components/RealInput.tsx
@@ -81,7 +81,7 @@ const RealInput = ({ type, name, placeholder,value,onChange,label }: InputField)
         name={name}
         placeholder={placeholder}
         className={`
-          min-w-[108%]
+          min-w-[100%]
           md:w-full
           px-4 py-3
           text-sm sm:text-base
