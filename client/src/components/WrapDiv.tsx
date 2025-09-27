1db9f06e86b100facf6bcbc2ade48291ce7003c2
elo369 <rohitrikam88@gmail.com>
2025-09-28 01:38:19 +0530

    jdfd

--- client/src/components/WrapDiv.tsx
+++ client/src/components/WrapDiv.tsx
@@ -4,7 +4,7 @@ const WrapDiv = ({children,id}:any) => {
   return (
     <div className="flex w-full flex-wrap gap-4 m-5 md:px-3 px-1">
   {/* Section Heading */}
-  <div className="w-full bg-gradient-to-r from-blue-600 via-gray-100 to-white  text-gray-800 font-semibold text-lg sm:text-xl md:text-2xl rounded-lg px-4 py-2 font-poppins tracking-wide">
+  <div className="w-[80vw] md:w-full bg-gradient-to-r from-blue-600 via-gray-100 to-white  text-gray-800 font-semibold text-lg sm:text-xl md:text-2xl rounded-lg px-4 py-2 font-poppins tracking-wide">
     {id.toUpperCase()}
   </div>
 
