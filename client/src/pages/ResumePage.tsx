6fd346b79663c743d07fe8a966fe3f70186762ee
elo369 <rohitrikam88@gmail.com>
2025-10-09 00:28:22 +0530

    add resume image

--- client/src/pages/ResumePage.tsx
+++ client/src/pages/ResumePage.tsx
@@ -13,6 +13,20 @@ import TemplateNine from '../resumeTemplate/TemplateNine'
 import TemplateTen from '../resumeTemplate/TemplateTen'
 import { useSelector } from 'react-redux'
 import type { RootState } from "../store/store";
+
+import resume1 from '../assets/pdf1_page-0001.jpg'
+import resume2 from '../assets/pdf2_page-0001.jpg'
+import resume3 from '../assets/pdf3_page-0001.jpg'
+import resume4 from '../assets/pdf4_page-0001.jpg'
+import resume5 from '../assets/pdf5_page-0001.jpg'
+import resume6 from '../assets/pdf6_page-0001.jpg'
+import resume7 from '../assets/pdf7_page-0001.jpg'
+import resume8 from '../assets/pdf8_page-0001.jpg'
+import resume9 from '../assets/pdf9_page-0001.jpg'
+import resume10 from '../assets/pdf10_page-0001.jpg'
+import resume11 from '../assets/pdf11_page-0001.jpg'
+
+
 const ResumePage = () => {
 
   const {template} = useSelector(
@@ -21,7 +35,8 @@ const ResumePage = () => {
   console.log(template)
   console.log("template")
 
-  
+  const resumeImage = [resume1,resume2,resume3,resume4,resume5,resume6,resume7,resume8,resume9,resume10,resume11]
+
   const renderTemplate = () => {
   switch (template) {
     case "TemplateOne":
@@ -52,7 +67,7 @@ const ResumePage = () => {
       <InputResume/>
       <div className='flex-col justify-center'>
           {renderTemplate()}
-            <FrontResume/>
+            {/* <FrontResume/>
            <TemplateOne/>
            <TemplateThree/>
            <TemplateTwo/>
@@ -62,7 +77,14 @@ const ResumePage = () => {
            <TemplateSeven/>
            <TemplateEight/>
            <TemplateNine />
-           <TemplateTen/>
+           <TemplateTen/> */}
+           <div className='grid grid-cols-2 md:grid-cols-3'>
+           {resumeImage.map((resumeImg,idx:number)=>(
+               <div key={idx}>
+                <img src={resumeImg} alt="resumeImage" className=' w-100' />
+               </div>
+           ))}
+           </div>
       </div>
     </div>
   )
