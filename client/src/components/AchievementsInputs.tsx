cabb593c44ca4b7e2284e9f87974425f2f8150d9
elo369 <rohitrikam88@gmail.com>
2025-10-18 23:36:16 +0530

    sdjsdjs

--- client/src/components/AchievementsInputs.tsx
+++ client/src/components/AchievementsInputs.tsx
@@ -7,60 +7,70 @@ import type { AppDispatch, RootState } from "../store/store";
 import { useDispatch, useSelector } from "react-redux";
 import { updateDataResume } from "../store/slice/resume/resume.slice";
 
- type Achievement = {
-    title: string;
-    explan: string;
-  };
+type Achievement = {
+  title: string;
+  explan: string;
+};
 
 type achievementObj = {
-  defaultAchieve:object;
-  dynamicAchieve:Achievement[];
-}
+  defaultAchieve: object;
+  dynamicAchieve: Achievement[];
+};
 
 const AchievementsInputs = () => {
+  const { collectData } = useSelector(
+    (state: RootState) => state.resumeReducer
+  );
 
-  const {collectData} = useSelector(
-    (state:RootState)=> (state.resumeReducer)
-  )
+  const [mainAchievement, setMainAchievement] = useState<{
+    [key: string]: string;
+  }>({});
+  const [achievement, setAchievement] = useState<Achievement[]>([
+    // {
+    //   title: "",
+    //   explan: "",
+    // },
+  ]);
 
-     const [mainAchievement,setMainAchievement] = useState<{[key:string]:string}>({})
-     const [achievement, setAchievement] = useState<Achievement[]>([
-        // {
-        //   title: "",
-        //   explan: "",
-        // },
-      ]);
-
-      useEffect(()=>{
-        if (collectData.achievements.defaultAchieve && Object.keys(mainAchievement).length == 0 || collectData.achievements.dynamicAchieve || Object.keys(achievement).length == 0) {
-          setMainAchievement(collectData.achievements.defaultAchieve as {[key:string]:string} )
-          setAchievement(collectData.achievements.dynamicAchieve as Achievement[] )
-        }
-      },[])
-
-       let collectAchievement = (e:React.ChangeEvent<HTMLInputElement>)=>{
-         setMainAchievement((prev)=>({
-            ...prev,
-            [e.target.name] : e.target.value
-         }))
+  useEffect(() => {
+    if (
+      (collectData.achievements.defaultAchieve &&
+        Object.keys(mainAchievement).length == 0) ||
+      collectData.achievements.dynamicAchieve ||
+      Object.keys(achievement).length == 0
+    ) {
+      setMainAchievement(
+        collectData.achievements.defaultAchieve as { [key: string]: string }
+      );
+      setAchievement(collectData.achievements.dynamicAchieve as Achievement[]);
     }
+  }, []);
 
-    let achievementObject:achievementObj = {
-      defaultAchieve : mainAchievement,
-      dynamicAchieve : [...achievement]
+  let collectAchievement = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMainAchievement((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  let achievementObject: achievementObj = {
+    defaultAchieve: mainAchievement,
+    dynamicAchieve: [...achievement],
+  };
+
+  let dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (
+      Object.keys(mainAchievement).length > 0 ||
+      Object.keys(achievement).length > 0
+    ) {
+      dispatch(updateDataResume({ achievements: achievementObject }));
     }
+  }, [mainAchievement, achievement]);
 
-    let dispatch = useDispatch<AppDispatch>()
+  console.log(mainAchievement, achievement);
 
-
-    useEffect(()=>{
-      if (Object.keys(mainAchievement).length > 0 || Object.keys(achievement).length > 0) {    
-        dispatch(updateDataResume({achievements:achievementObject}))
-      }
-    },[mainAchievement,achievement])
-
-    console.log(mainAchievement,achievement)
-    
   return (
     <div className=" min-h-[70vh]">
       <WrapDiv id="Achievements">
@@ -80,44 +90,44 @@ const AchievementsInputs = () => {
           value={mainAchievement.achievementDescription || ""}
           onChange={collectAchievement}
         />
-        </WrapDiv>
-        {achievement.map((desc, index) => {
-          return (
-            <div key={index} className="flex flex-wrap gap-3 ">
-              <RealInput
-                type="text"
-                name={`achievementTitle${index + 1}`}
-                label="achievement Title"
-                placeholder="Achievement Title"
-                value={desc.title || ""}
-                onChange={(e) => {
-                  const inputObj = [...achievement];
-                  inputObj[index] = {
-                    ...inputObj[index],
-                    title: e.target.value,
-                  };
-                  setAchievement(inputObj);
-                }}
-              />
-              <RealInput
-                type="text"
-                name={`achievementDescription ${index + 1}`}
-                label="achievement Description"
-                placeholder="Description of Achievement"
-                value={desc.explan || ""}
-                onChange={(e) => {
-                  const inputObj = [...achievement];
-                  inputObj[index] = {
-                    ...inputObj[index],
-                    explan: e.target.value,
-                  };
-                  setAchievement(inputObj);
-                }}
-              />
-            </div>
-          );
-        })}
-        <div className="flex gap-2 pl-5">
+      </WrapDiv>
+      {achievement.map((desc, index) => {
+        return (
+          <div key={index} className="flex flex-wrap gap-3 mt-2 pl-4">
+            <RealInput
+              type="text"
+              name={`achievementTitle${index + 1}`}
+              label="achievement Title"
+              placeholder="Achievement Title"
+              value={desc.title || ""}
+              onChange={(e) => {
+                const inputObj = [...achievement];
+                inputObj[index] = {
+                  ...inputObj[index],
+                  title: e.target.value,
+                };
+                setAchievement(inputObj);
+              }}
+            />
+            <RealInput
+              type="text"
+              name={`achievementDescription ${index + 1}`}
+              label="achievement Description"
+              placeholder="Description of Achievement"
+              value={desc.explan || ""}
+              onChange={(e) => {
+                const inputObj = [...achievement];
+                inputObj[index] = {
+                  ...inputObj[index],
+                  explan: e.target.value,
+                };
+                setAchievement(inputObj);
+              }}
+            />
+          </div>
+        );
+      })}
+      <div className="flex gap-2 pl-5 mt-2 ">
         <button
           className="h-10 w-10 bg-blue-700 border-amber-200  rounded-md "
           onClick={() =>
@@ -140,9 +150,9 @@ const AchievementsInputs = () => {
         >
           -
         </button>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AchievementsInputs
+export default AchievementsInputs;
